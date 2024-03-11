402a08c201c90cbd794c08bd198177b77af40f84
SzybkiRabarbar <maksjoni36@gmail.com>
2024-03-11 18:00:56 +0100

    Front: Create and connect Groups, GroupDetail and Article pages

--- frontend/awe/src/pages/groups/Groups.tsx
+++ frontend/awe/src/pages/groups/Groups.tsx
@@ -1,6 +1,7 @@
 import './Groups.scss';
 
 import GroupsDataInterface from '../../interfaces/GroupsDataInterface';
+import { Link } from 'react-router-dom';
 
 function Groups(props: { groupsData: GroupsDataInterface; }) {
   const groupsData = props.groupsData;
@@ -8,7 +9,13 @@ function Groups(props: { groupsData: GroupsDataInterface; }) {
   return (
     <div className='Groups'>
       {Array.isArray(groupsData.groups) &&
-      groupsData.groups.map(obj => [obj.id, obj.name])
+      groupsData.groups.map((obj, index) => (
+        <div key={ index } className='group-item'>
+          <Link to={"/group/" + obj.id} target='_self'>
+            <h3>{ obj.name }</h3>
+          </Link>
+        </div>
+      ))
       }
     </div>
   );
