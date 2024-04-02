fe071f24aa373055f266d74c81adbd0b7f63a3aa
SzybkiRabarbar <maksjoni36@gmail.com>
2024-04-02 19:07:51 +0200

    Back: Fix token validation

--- backend/awe/users_progress/validate_token/validate_token.py
+++ backend/awe/users_progress/validate_token/validate_token.py
@@ -1,34 +1,42 @@
-import json
-import jwt
-import requests
-from rest_framework.response import Response
-from rest_framework import status
+# import json
+# import firebase_admin
+from firebase_admin import auth
 
 
 class ValidateToken:
 
-    with open('users_progress/creds/identity-platform.json', 'r') as file:
-        CLIENTID = json.load(file)['clientId']
+    # with open('users_progress/creds/identity-platform.json', 'r') as file:
+    #     content  = json.load(file)
+    #     CLIENT_ID = content['clientId']
+
+    # with open('users_progress/creds/cred-firebase-adminsdk.json', 'r') as file:
+    #     content = json.load(file)
+    #     CLIENT_ID = content['client_id']
 
     @staticmethod
     def decode(token):
-
-        # Get Google's public keys
-        keys = requests.get('https://www.googleapis.com/oauth2/v1/certs').json()
-        header = jwt.get_unverified_header(token)
-        public_key = keys[header['kid']]
-
+        
         try:
-            # Validate the token
-            payload = jwt.decode(
-                token, 
-                public_key, 
-                algorithms='RS256', 
-                audience=ValidateToken.CLIENTID,
-                issuer='https://accounts.google.com'
-            )
-            return payload
-        except jwt.ExpiredSignatureError:
-            return Response({'error': 'Token expired'}, status=status.HTTP_401_UNAUTHORIZED)
-        except jwt.InvalidTokenError:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+            # Verify the ID token while checking if the token is revoked by
+            # passing check_revoked=True.
+            decoded_token = auth.verify_id_token(
+                token, check_revoked=True)
+
+            # Token is valid and not revoked.
+            return decoded_token
+
+        except ValueError:
+            # Token was not a string or was empty.
+            return 'The token was not a string or was empty.'
+        except auth.InvalidIdTokenError:
+            # Token is invalid
+            return 'The token is invalid.'
+        except auth.ExpiredIdTokenError:
+            # Token is expired
+            return 'The token is expired.'
+        except auth.RevokedIdTokenError:
+            # Token has been revoked
+            return 'The token has been revoked.'
+        except auth.CertificateFetchError:
+            # Could not fetch the certificates required to verify the token
+            return 'Could not fetch the certificates required to verify the token.'
--- backend/awe/users_progress/views.py
+++ backend/awe/users_progress/views.py
@@ -5,20 +5,22 @@ from users_progress.validate_token.validate_token import ValidateToken
 
 class RetrieveUserAction(APIView):
 
-    def post(self, request):
+    def patch(self, request):
         token = request.META.get('HTTP_AUTHORIZATION', None)
-        group_id = request.data.get('group_id')
-        fire_id = request.data.get('fire_id')
-        action_number = request.data.get('action_number')
+        group_id = request.data.get('groupId')
+        fire_id = request.data.get('fireId')
+        action_number = request.data.get('actionNumber')
 
         if not all([token, group_id, fire_id, action_number]):
-            return Response({'error': 'Missing required parameters'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Missing required parameters'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-        payload = ValidateToken.decode(token)
-        if isinstance(payload, Response):  # | If error occurred
-            return payload
+        payload = ValidateToken.decode(token.split(' ')[1]
+                                       if ' ' in token else None)
+        if isinstance(payload, str):  # | If error occurred
+            return Response({'error:': payload},
+                            status=status.HTTP_401_UNAUTHORIZED)
 
-        # If token is valid, continue processing the request
-        # Here you can add your logic to process the data
+        print(payload['user_id'])
 
         return Response({'status': 'success'}, status=status.HTTP_200_OK)
